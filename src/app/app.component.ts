c32dcfe2262d4031262afd74f68e56965646f2b1
LeonardoCoutin <40408797+LeonardoCoutin@users.noreply.github.com>
2019-04-01 17:47:24 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'login',
+      url: '/login',
+      icon: 'contact'
     }
   ];
 
